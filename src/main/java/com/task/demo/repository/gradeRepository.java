0791daf156e905da932dd889335428a5540333b3
kirSofya <116517600+kirSofya@users.noreply.github.com>
2023-06-17 15:48:33 +0300

    Update gradeRepository.java

--- src/main/java/com/task/demo/repository/gradeRepository.java
+++ src/main/java/com/task/demo/repository/gradeRepository.java
@@ -2,7 +2,7 @@ package com.task.demo.repository;
 
 import com.task.demo.exception.NotFoundException;
 import com.task.demo.model.grades;
-import com.task.demo.model.students;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,18 +10,18 @@ import java.util.Map;
 public class gradeRepository {
 
     // Оценки
-    private static final Map<Integer, students> GRADES_REPOSITORY_MAP = new HashMap<>();
+    private static final Map<Integer, Integer> GRADES_REPOSITORY_MAP = new HashMap<>();
 
     // Выставить оценку
     @Override
     public void save(grades grade) throws NotFoundException {
         final int studentId = Math.toIntExact(grade.getId());
-        GRADES_REPOSITORY_MAP.put(studentId, grade.getSubject());
+        GRADES_REPOSITORY_MAP.put(studentId, studentId);
     }
 
     // Получить оценку
     @Override
-    public grades findById(int id) {
+    public Integer findById(int id) {
         return GRADES_REPOSITORY_MAP.get(id);
     }
 }
